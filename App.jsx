8df177c509b5b80b1a62a7863bd628ef2afed81a
Gunakabilan <gunaseelan.mtm@gmail.com>
2025-03-07 15:07:12 +0530

    Usestate Hook

--- App.jsx
+++ App.jsx
@@ -1,18 +1,24 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const App = () => {
+  const [num, setNum] = useState(1);
   let x=1;
-    const handleAdd = ()=>{
-      x=x+1;
-      console.log("x")
-    }
+    const handleAdd = ()=> {
+      //x=x+1;
+      //console.log("x",x)
+      //setNum(10)
+      setNum((currval) => {
+        return currval +1
+      })
+    };
+    console.log(num);
 
   return (
     <div>
-      <h1>{x}</h1>
-      <button>Add</button>
+      <h1>{num}</h1>
+      <button onClick={handleAdd}>Add</button>
     </div>
   )
-}
+}    
 
 export default App
